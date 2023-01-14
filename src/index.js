ba463bd5822a68bac8669b8a193e37544b19bebe
Artem07199 <artemlitasov@gmail.com>
2023-01-14 14:37:59 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let arr;
+  if(n < 0){
+   arr = n.toString().slice(1).split(''); 
+  }else{
+
+    arr = n.toString().split('');
+  }
+  arr.reverse();
+  return Number(arr.join('')) 
 }
